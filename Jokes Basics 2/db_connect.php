e540d68d4b9d2f2547cbcaf024a16c72351bd617
Quagge1 <108851187+Quagge1@users.noreply.github.com>
2023-10-28 18:36:39 -0700

    Update db_connect.php

--- Jokes Basics 2/db_connect.php
+++ Jokes Basics 2/db_connect.php
@@ -4,11 +4,11 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 // modify these settings according to the account on your database server.
-$host = "localhost";
+$host = "qao3ibsa7hhgecbv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $port = "3306";
-$username = "root";
-$user_pass = "root";
-$database_in_use = "JokesPart1";
+$username = "aon5mhbco0cnzp8h";
+$user_pass = "yhhduha79mccyx4z";
+$database_in_use = "xy5jndpa0k43wq8d";
 
 
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
